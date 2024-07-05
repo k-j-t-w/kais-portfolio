2936c24b90af2c5313eacd893db65082bcd89fed
Kai Wingfield <kaiwingfield17@gmail.com>
2024-07-04 21:17:09 -0700

    add blur effect to nav bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,11 +20,13 @@ function Home({}) {
             <div className='social'>Linkedin</div>
             <div className='social'>Email</div>
           </div>
-          <h2>Work.</h2>
+          <h2 id='work'>Work.</h2>
           <Projects/>
-          <h2>About.</h2>
-          <div>this is the about me paragraph</div>
-          <h2>Get in touch</h2>
+          <h2 id='about'>About.</h2>
+          <div className='about-para'>With a diploma in web development, a certificate in graphic design, and a background rooted in the art and print industry, my passion for design and technology has been a lifelong pursuit. From early beginnings, I've been immersed in the world behind screens, meticulously deconstructing designs and websites to uncover their craftsmanship.</div>
+          <div className='about-para'>My journey started as a freelance web designer and developer while pursuing my education. Over time, this evolved into a deep-seated passion for creating impactful digital solutions. I thrive on the challenge of blending creativity with technical expertise, crafting seamless and innovative user experiences.</div>
+          <div className='about-para'>For the past years, I have dedicated myself to full-time web development, collaborating with diverse clients and projects that continually expand my skill set. I prioritize honesty and transparency in my work, finding fulfillment in partnering with like-minded companies and teams to build exceptional digital products.</div>
+          <h2 id='contact'>Get in touch</h2>
           <div className='home-socials'>
             <div className='social'>Github</div>
             <div className='social'>Linkedin</div>
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,16 +3,23 @@ import '../styles/TopNav.css'
 
 function TopNav({}) {
 
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className ='top-nav'>
     <div className='top-nav-span'>
       <div className='top-nav-logo'>Kai Wingfield.</div>
 
       <div className='top-nav-items'>
-        <div className='top-nav-item'>Work</div>
-        <div className='top-nav-item'>About</div>
+        <div className='top-nav-item' onClick={() => scrollToSection('work')}>Work</div>
+        <div className='top-nav-item' onClick={() => scrollToSection('about')}>About</div>
         <div className='top-nav-item'>Resume</div>
-        <div className='top-nav-item'>Contact</div>
+        <div className='top-nav-item' onClick={() => scrollToSection('contact')}>Contact</div>
         <div className='top-nav-item'>dark</div>
       </div>
       
